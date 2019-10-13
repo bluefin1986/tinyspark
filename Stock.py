46825a0c83baadf02c8ca10058dece74cd908806
bluefin1986 <zhengminjia@cibfintech.com>
2019-10-13 17:06:09 +0800

    RSI能够直接筛出不含ST、退市股的计算结果

--- Stock.py
+++ Stock.py
@@ -5,14 +5,15 @@
 
 
 class Stock(object):
-    code = None,
-    name = None,
+    code = None
+    name = None
     tradeStatus = 1
+    stockType = None
+    isST = False
     
-    def __init__(self,code,name,tradeStatus):
+    def __init__(self,code,name):
         self.code = code
         self.name = name
-        self.tradeStatus = tradeStatus
         
     def __str__(self):
         return '(%s %s %s)' %(self.code, self.name, self.tradeStatus)
